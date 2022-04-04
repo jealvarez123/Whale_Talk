912c74b9fe7c8684e3adce5594e0160c60aeede5
Jaime Alvarez <jealvarez123@gmail.com>
2022-04-04 16:03:41 -0500

    completed the project

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ const input = 'Can you give us directions to Sydney, Australia';
 const vowels = ['a', 'e', 'i', 'o', 'u'];
 const resultArray = [];
 
-for (i = 0; i < input.length; i++) {
-  for (j = 0; j < vowels.length; j++) {
+for (let i = 0; i < input.length; i++) {
+  if (input[i] === 'e'|| input[i] === 'u') {
+    resultArray.push(input[i])
+  }
+  for (let j = 0; j < vowels.length; j++) {
     if (input[i] === vowels[j]) {
-      console.log(input[i])
+      resultArray.push(input[i])
     }
   }
 };
+
+console.log(resultArray.join('').toUpperCase());
